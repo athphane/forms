d5365c63f82709dea5b6c81f47ef346930c27a4d
athphane <athphane@gmail.com>
2024-02-27 15:04:06 +0500

    - bs5 test for bulk-actions

--- tests/Feature/BulkTest.php
+++ tests/Feature/BulkTest.php
@@ -16,7 +16,27 @@ class BulkTest extends TestCase
             ->seeElement('select')
             ->seeElement('option[value="approve"]')
             ->seeElement('option[value="ban"]')
-            ->seeElement('option[value="delete"]');
+            ->seeElement('option[value="delete"]')
+            ->seeElement('button[type="submit"]')
+            ->within('button[type="submit"]', function () {
+                $this->see('Apply');
+            });
+    }
 
+    /** @test */
+    public function it_can_generated_bulk_actions_for_bootstrap_5()
+    {
+        $this->setFrameworkBootstrap5();
+        $this->registerTestRoute('bulk-actions');
+
+        $this->visit('/bulk-actions')
+            ->seeElement('select')
+            ->seeElement('option[value="approve"]')
+            ->seeElement('option[value="ban"]')
+            ->seeElement('option[value="delete"]')
+            ->seeElement('button[type="submit"]')
+            ->within('button[type="submit"]', function () {
+                $this->see('Apply');
+            });
     }
 }
